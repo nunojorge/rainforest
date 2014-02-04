3a9bd7d3ecdca1a713f48ae9d41c4075b031850b
nunojorge <nujo@hotmail.com>
2014-02-03 20:00:53 -0500

    Creating a user model, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rainforest::Application.routes.draw do
+  get "users/new"
+  get "users/create"
   resources :products
-  root :to => "products#index"
+  resources :users, :only => [:new, :create]
+  #root :to => "products#index"
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
